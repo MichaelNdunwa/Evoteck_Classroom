7ffd18ccc0920fb435adca56aa9199d146dd2b16
MichaelNdunwa <ndunwa240@gmail.com>
2025-05-29 15:18:26 +0100

    re-arranged the files in core/auth

--- app/src/main/java/com/evoteckgeospatialconsult/core/auth/AuthResult.kt
+++ app/src/main/java/com/evoteckgeospatialconsult/core/auth/AuthResult.kt
@@ -1,7 +1,7 @@
 package com.evoteckgeospatialconsult.core.auth
 
 import com.evoteckgeospatialconsult.core.auth.model.AuthError
-import com.evoteckgeospatialconsult.core.auth.models.AuthUser
+import com.evoteckgeospatialconsult.core.auth.model.AuthUser
 
 sealed class AuthResult {
     data class Success(val user: AuthUser?) : AuthResult()
