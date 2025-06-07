2eb5392a5f6f07785c47b18ccd70c2ecc571a914
MichaelNdunwa <ndunwa240@gmail.com>
2025-06-07 08:16:25 +0100

    set statusBar colors

--- app/src/main/java/com/evoteckgeospatialconsult/MainActivity.kt
+++ app/src/main/java/com/evoteckgeospatialconsult/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
